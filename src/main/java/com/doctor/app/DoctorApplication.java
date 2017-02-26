fe0b8562d63e5ea0048d7a33648b726bbe546712
Neeraj Girolkar <girolkar.neeraj@gmail.com>
2017-02-26 18:51:40 +0530

    Updating build.gradle to add support to prepare WAR files

--- src/main/java/com/doctor/app/DoctorApplication.java
+++ src/main/java/com/doctor/app/DoctorApplication.java
@@ -2,10 +2,17 @@ package com.doctor.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DoctorApplication {
+public class DoctorApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DoctorApplication.class, args);
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DoctorApplication.class);
     }
 }
